739fb532f130e7120742d4779df8ff6c76ad7c6e
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-11-25 11:14:27 -0500

    Se cambian tamaños en el diseño de incio de sesion

--- views/src/js/scriptLoginRegisterUser.js
+++ views/src/js/scriptLoginRegisterUser.js
@@ -5,11 +5,13 @@ const abrirRegistro = document.getElementById('abrir-registro');
 abrirLogin.addEventListener('click', () => {
     formLogin.classList.add('abrir-login');
     formLogin.classList.remove('cerrar-login');
+    abrirRegistro.classList.add('titulo-registro-cerrar');
 });
 
 abrirRegistro.addEventListener('click', () => {
     formLogin.classList.add('cerrar-login');
     formLogin.classList.remove('abrir-login');
+    abrirRegistro.classList.remove('titulo-registro-cerrar');
 });
 
 
